5a23629bbec6994270ba318e66706b8bf1cd3feb
Giovanni Fabris <giovanni.fabris@student.h-farm.com>
2020-12-01 16:51:09 +0100

    various mods

--- database.py
+++ database.py
@@ -1,8 +1,11 @@
 import csv
-import guitarists
+from guitarists import list_of_guitarists
+
 def database(dictionary):
-	a_file = open("guitarists.csv","w")
-    writer = csv.writer(a_file)
-    for key, value in dictionary.items():
-        writer.writerrow([key,value])
-    a_file_close()
+        a_file = open("guitarists.csv","w")
+        writer = csv.writer(a_file)
+        for key, value in dictionary.items():
+            writer.writerow([key,value])
+        a_file.close()
+
+database(list_of_guitarists)
