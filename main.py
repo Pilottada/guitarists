0d6bf1154f45240bff6b9ddd2827b86a70e86050
Giovanni Fabris <giovanni.fabris@student.h-farm.com>
2020-12-01 16:38:54 +0100

    modified main.py

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 #! /usr/bin/env python3
 
-from guitarists import check_guitarist, check_band
+from guitarists import check_guitarist, check_band, adding_process
 
 check_guitarist("Kirk Hammet")
 check_guitarist("Young Signorino")
 check_band("Guns'n Roses")
 check_band("Ricchi e Poveri")
+adding_process()
+
 
